c1d0fcc3ae8ce3b28036574aa504192cc483d6c9
Asif Taj <axiftaj@gmail.com>
2024-12-31 22:08:28 +0500

    changes done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,10 +4,13 @@ import 'package:riverpod_tutorial/counter_provider.dart';
 
 import 'package:riverpod/riverpod.dart';
 
-final counterProvider = Provider<String>((ref) {
-  return 'Subscriber' ;
+final counterProvider = StateProvider<int>((ref) {
+  return  0 ;
 });
 
+final switchProvider = StateProvider<bool>((ref) {
+  return  false ;
+});
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -20,20 +23,59 @@ class _HomePageState extends ConsumerState<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final value = ref.watch(counterProvider);
-    return ProviderScope(
-      child: Scaffold(
-        appBar: AppBar(title: const Text("Riverpod Basics")),
-        body: Center(
-          child: Text(value.toString()),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: (){
-             // ref.read(counterProvider.notifier).state++;
-            }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App with Riverpod'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+              style: TextStyle(fontSize: 16),
+            ),
+            Consumer(
+                builder: (context, ref, child){
+                  final counter = ref.watch(counterProvider);
+                  return Text(
+                    '$counter',
+                    style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  );
+                }
+            ),
+            Consumer(
+                builder: (context, ref, child){
+                  final counter = ref.watch(switchProvider);
+                  return Switch(value: counter, onChanged: (value){
+                    ref.read(switchProvider.notifier).state = value;
+                  });
+                }
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counterProvider.notifier).state++;
+                  },
+                  child: const Text('+'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counterProvider.notifier).state--;
+                  },
+                  child: const Text('-'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+
   }
 }
 
